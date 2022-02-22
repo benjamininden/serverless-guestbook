7a15085c9f2407984842768c6ef8eb7a5c6ebdda
benjamininden <41686471+benjamininden@users.noreply.github.com>
2022-02-22 11:34:22 +0100

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://e4d9d95f.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
